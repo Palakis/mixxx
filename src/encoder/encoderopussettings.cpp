7067ac91c394d7fe062a735add6abfd6e5320206
Stéphane Lepin <stephane.lepin@gmail.com>
2017-08-15 11:16:14 +0200

    Basic features in EncoderOpusSettings

--- src/encoder/encoderopussettings.cpp
+++ src/encoder/encoderopussettings.cpp
@@ -2,47 +2,84 @@
 // Create on August 15th 2017 by Palakis
 
 #include "encoder/encoderopussettings.h"
+#include "recording/defs_recording.h"
+#include "util/logger.h"
+
+namespace {
+const int kDefaultBitrateIndex = 6;
+const char* kQualityKey = "Opus_Quality";
+const mixxx::Logger kLogger("EncoderOpusSettings");
+}
 
 EncoderOpusSettings::EncoderOpusSettings(UserSettingsPointer pConfig)
     : m_pConfig(pConfig) {
+    m_qualList.append(32);
+    m_qualList.append(48);
+    m_qualList.append(64);
+    m_qualList.append(80);
+    m_qualList.append(96);
+    m_qualList.append(112);
+    m_qualList.append(128);
+    m_qualList.append(160);
+    m_qualList.append(192);
+    m_qualList.append(224);
+    m_qualList.append(256);
+    m_qualList.append(320);
 }
 
 EncoderOpusSettings::~EncoderOpusSettings() {
 }
 
 QList<int> EncoderOpusSettings::getQualityValues() const {
-    return QList<int>();
-}
-
-QList<int> EncoderOpusSettings::getVBRQualityValues() const {
-    return QList<int>();
+    return m_qualList;
 }
 
 void EncoderOpusSettings::setQualityByValue(int qualityValue) {
+    // Same behavior as Vorbis: Opus does not have a fixed set of
+    // bitrates, so we can accept any value.
+    int indexValue;
+    if (m_qualList.contains(qualityValue)) {
+        indexValue = m_qualList.indexOf(qualityValue);
+    } else {
+        // If we let the user write a bitrate value, this would allow to save such value.
+        indexValue = qualityValue;
+    }
 
+    m_pConfig->setValue<int>(ConfigKey(RECORDING_PREF_KEY, kQualityKey), indexValue);
 }
 
 void EncoderOpusSettings::setQualityByIndex(int qualityIndex) {
-
+    if (qualityIndex >= 0 && qualityIndex < m_qualList.size()) {
+        m_pConfig->setValue<int>(ConfigKey(RECORDING_PREF_KEY, kQualityKey), qualityIndex);
+    } else {
+        kLogger.warning() << "Invalid qualityIndex given to EncoderVorbisSettings: "
+                          << qualityIndex << ". Ignoring it";
+    }
 }
-
 int EncoderOpusSettings::getQuality() const {
-    return 0;
+    int qualityIndex = m_pConfig->getValue(
+            ConfigKey(RECORDING_PREF_KEY, kQualityKey), kDefaultBitrateIndex);
+
+    if (qualityIndex >= 0 && qualityIndex < m_qualList.size()) {
+        return m_qualList.at(qualityIndex);
+    } else {
+        // Same as Vorbis: Opus does not have a fixed set of
+        // bitrates, so we can accept any value.
+        return qualityIndex;
+    }
 }
 
 int EncoderOpusSettings::getQualityIndex() const {
-    return -1;
+    int qualityIndex = m_pConfig->getValue(
+            ConfigKey(RECORDING_PREF_KEY, kQualityKey), kDefaultBitrateIndex);
+
+    if (qualityIndex >= 0 && qualityIndex < m_qualList.size()) {
+        return qualityIndex;
+    } else {
+        kLogger.warning() << "Invalid qualityIndex in EncoderVorbisSettings "
+                          << qualityIndex << "(Max is:"
+                          << m_qualList.size() << ") . Ignoring it and"
+                          << "returning default which is" << kDefaultBitrateIndex;
+        return kDefaultBitrateIndex;
+    }
 }
-
-QList<EncoderSettings::OptionsGroup> EncoderOpusSettings::getOptionGroups() const {
-    return QList<EncoderSettings::OptionsGroup>();
-}
-
-void EncoderOpusSettings::setGroupOption(QString groupCode, int optionIndex) {
-
-}
-
-int EncoderOpusSettings::getSelectedOption(QString groupCode) const {
-    return -1;
-}
-
--- src/encoder/encoderopussettings.h
+++ src/encoder/encoderopussettings.h
@@ -27,7 +27,6 @@ class EncoderOpusSettings: public EncoderSettings {
 
     // Returns the list of quality values that it supports, to assign them to the slider
     QList<int> getQualityValues() const override;
-    QList<int> getVBRQualityValues() const;
     // Sets the quality value by its value
     void setQualityByValue(int qualityValue) override;
     // Sets the quality value by its index
@@ -35,17 +34,10 @@ class EncoderOpusSettings: public EncoderSettings {
     // Returns the current quality value
     int getQuality() const override;
     int getQualityIndex() const override;
-    // Returns the list of radio options to show to the user
-    QList<EncoderSettings::OptionsGroup> getOptionGroups() const override;
-    // Selects the option by its index. If it is a single-element option,
-    // index 0 means disabled and 1 enabled.
-    void setGroupOption(QString groupCode, int optionIndex) override;
-    // Return the selected option of the group. If it is a single-element option,
-    // 0 means disabled and 1 enabled.
-    int getSelectedOption(QString groupCode) const override;
 
   private:
     UserSettingsPointer m_pConfig;
+    QList<int> m_qualList;
 };
 
 #endif // ENCODER_ENCODEROPUSSETTINGS_H
